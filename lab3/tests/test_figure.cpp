1ec4caa7c94b36f4e95e197e60057e2cbba6b599
zmskv <zmskvxd@gmail.com>
2024-10-09 00:04:50 +0300

    done

--- lab3/tests/test_figure.cpp
+++ lab3/tests/test_figure.cpp
@@ -3,13 +3,13 @@
 #include "../src/hexagon.cpp"
 #include "../src/pentagon.cpp"
 
-TEST(RhombusTest, ConstructorAndEquality)
+TEST(RhombusTest, Constructor)
 {
     Point points[4] = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
     Rhombus rhombus(points);
 
+    ASSERT_NO_THROW(rhombus.getInfo());
     ASSERT_NO_THROW(rhombus.geometricCenter());
-    EXPECT_EQ(rhombus, rhombus);
 }
 
 TEST(RhombusTest, InvalidRhombus)
@@ -19,13 +19,13 @@ TEST(RhombusTest, InvalidRhombus)
     EXPECT_THROW(Rhombus rhombus(points), std::invalid_argument);
 }
 
-TEST(PentagonTest, ConstructorAndEquality)
+TEST(PentagonTest, Constructor)
 {
     Point points[5] = {{1.0, 0.0}, {0.309, 0.951}, {-0.809, 0.588}, {-0.809, -0.588}, {0.309, -0.951}};
     Pentagon pentagon(points);
 
     ASSERT_NO_THROW(pentagon.getInfo());
-    EXPECT_EQ(pentagon, pentagon);
+    ASSERT_NO_THROW(pentagon.geometricCenter());
 }
 
 TEST(PentagonTest, InvalidPentagon)
@@ -35,13 +35,13 @@ TEST(PentagonTest, InvalidPentagon)
     EXPECT_THROW(Pentagon pentagon(points), std::invalid_argument);
 }
 
-TEST(HexagonTest, ConstructorAndEquality)
+TEST(HexagonTest, Constructor)
 {
     Point points[6] = {{1.0, 0.0}, {0.5, 0.866}, {-0.5, 0.866}, {-1.0, 0.0}, {-0.5, -0.866}, {0.5, -0.866}};
     Hexagon hexagon(points);
 
     ASSERT_NO_THROW(hexagon.getInfo());
-    EXPECT_EQ(hexagon, hexagon);
+    ASSERT_NO_THROW(hexagon.geometricCenter());
 }
 
 TEST(HexagonTest, InvalidHexagon)
